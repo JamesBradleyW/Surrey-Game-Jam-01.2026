fd25e2adb66f0a3f088ee339cc90ef7938e96ca2
StvnC12 <107701847+StvnC12@users.noreply.github.com>
2026-01-27 23:05:38 +0000

    Added Jump Buffering and Coyote Time
    Jump Buffering Timer to allow jump input before landing
    Coyote Timer to allow jump input after falling off ledge

--- src/scripts/player scripts/player_(character_body_2d).gd
+++ src/scripts/player scripts/player_(character_body_2d).gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@export_subgroup("Timers")
+@export var jump_buffer_timer: Timer
+@export var coyote_timer: Timer
+
 @export var speed = 300.0
 @export var jump_velocity = -400.0
 @export var acceleration = 15.0
@@ -8,7 +12,53 @@ extends CharacterBody2D
 # Get the gravity from the project settings
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_going_up = false
+var is_jumping = false 
+var on_floor_last_frame = false
+
 @onready var animated_sprite = $AnimatedSprite2D
+
+func has_just_landed(body: CharacterBody2D):
+	return body.is_on_floor() and is_jumping and not on_floor_last_frame
+
+func has_just_stepped_off_ledge(body: CharacterBody2D):
+	return not body.is_on_floor() and on_floor_last_frame and not is_jumping
+	
+func is_allowed_to_jump(body, jump_input):
+	return jump_input and (body.is_on_floor() or not coyote_timer.is_stopped())
+	
+func run_jump(body: CharacterBody2D):
+	body.velocity.y = jump_velocity
+	is_jumping = true
+	jump_buffer_timer.stop()
+	coyote_timer.stop()
+	
+func handle_jump(body: CharacterBody2D, jump_input: bool):
+	if has_just_landed(body):
+		is_jumping = false
+	
+	if is_allowed_to_jump(body, jump_input):
+		run_jump(body)
+		
+	handle_jump_buffer(body, jump_input)
+	handle_coyote_time(body)
+	
+	is_going_up = body.velocity.y < 0 and not body.is_on_floor()
+	on_floor_last_frame = body.is_on_floor()
+	
+func handle_jump_buffer(body: CharacterBody2D, jump_input: bool):
+	if jump_input and not body.is_on_floor():
+		jump_buffer_timer.start()
+	
+	if body.is_on_floor() and not jump_buffer_timer.is_stopped():
+		run_jump(body)
+		
+func handle_coyote_time(body: CharacterBody2D):
+	if has_just_stepped_off_ledge(body):
+		coyote_timer.start()
+		
+	if not coyote_timer.is_stopped() and not is_jumping:
+		body.velocity.y = 0
 
 func _physics_process(delta):
 	# Add gravity every frame
@@ -16,8 +66,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	handle_jump(self, Input.is_action_just_pressed("jump"))
 
 	# Get input direction
 	var direction = Input.get_axis("left", "right")
